3d69c7bb20bbe21859a533f6b257f7c7bc7c0fd3
Carlos Escutia <carlosescutia@gmail.com>
2021-11-01 17:14:25 -0600

    Se agregan todas las secciones del proyecto al script de construcción de pdf.

--- build/pdf.sh
+++ build/pdf.sh
@@ -3,9 +3,15 @@
 # pdf.sh
 # build book as pdf
 
-pandoc formato_pdf.yaml \
+pandoc \
+    formato_pdf.yaml \
     01_post_scriptum.md \
     LICENSE \
     02_introduccion.md \
+    03_roza_jaula_pajaro.md \
+    04_historia.md \
+    05_capoeira_angola_regional.md \
+    06_jogo_bonito.md \
+    07_canciones.md \
     --pdf-engine=xelatex \
     -o voltamundo.pdf
